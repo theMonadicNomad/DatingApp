1bff49332620ef5b994dca29319a4103b2c02c14
theMonadicNomad <hello@nagacharan.me>
2021-11-16 10:23:53 +0100

    6 before angular update to 13

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -21,7 +21,7 @@ export class RegisterComponent implements OnInit {
       console.log(response);
       this.cancel();
     },error => {
-      console.log(error);
+      console.log(error); 
     }
     )
   }
